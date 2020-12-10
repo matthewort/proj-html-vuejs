1e214bfed4ce51dfadded37080f7f135fe3532bf
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2020-12-10 18:30:52 +0100

    Aggiunta sfondo navbar con Vue

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,16 @@
 var app = new Vue({
   el: '#app',
   data: {
+    clock: 'fas fa-clock',
     hours: 'Open Hours: Mon - Sat - 9:00 - 18:00',
+    facebook: 'fab fa-facebook-f',
+    twitter: 'fab fa-twitter',
+    instagram: 'fab fa-instagram',
     info: [
       {message: '+1(305)1234-5678'},
       {message: 'hello@example.com'}
-    ]
+    ],
+    navimage: 'img/bg-4.jpg',
   },
   methods: {
     addfilm: function () {
