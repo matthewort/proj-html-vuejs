ef8b1cdec045b77c0595112c630b5d13a596ffce
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2020-12-11 11:02:26 +0100

    Footer

--- js/myscript.js
+++ js/myscript.js
@@ -1,31 +1,46 @@
 var app = new Vue({
   el: '#app',
   data: {
-    clock: 'fas fa-clock',
+    icons: [
+      {clock: 'fas fa-clock'},
+      {facebook: 'fab fa-facebook-f'},
+      {twitter: 'fab fa-twitter'},
+      {instagram: 'fab fa-instagram'},
+      {phone: 'fas fa-phone-alt'},
+      {envelope: 'fas fa-envelope'},
+      {position: 'fas fa-map-marker-alt'}
+    ],
     hours: 'Open Hours: Mon - Sat - 9:00 - 18:00',
-    facebook: 'fab fa-facebook-f',
-    twitter: 'fab fa-twitter',
-    instagram: 'fab fa-instagram',
     info: [
       {message: '+1(305)1234-5678'},
-      {message: 'hello@example.com'}
+      {message: 'hello@example.com'},
+      {message: 'Main Avenue, 987'}
     ],
     navimage: 'img/bg-4.jpg',
-  },
-  methods: {
-    addfilm: function () {
-      axios.get(film + this.message)
-      .then((response) => {
-      const filmcontent = response.data.results;
-      this.film = filmcontent
-      });
-    },
-    addseries: function () {
-      axios.get(series + this.message)
-      .then((response) => {
-      const seriescontent = response.data.results;
-      this.series = seriescontent
-      });
-    }
+    nexgen: 'NEXGEN',
+    navtext: [
+      {text: 'HOME'},
+      {text: 'ABOUT'},
+      {text: 'PROJECTS'},
+      {text: 'PROCESS'},
+      {text: 'TESTIMONIALS'}
+    ],
+    touch: 'GET IN TOUCH',
+    user: 'fas fa-user',
+    // paragraphleft: [
+    //   {par: 'SEND A MESSAGE'},
+    //   {par: 'Get in Touch'},
+    //   {par: 'We will respond to your message as soon as possible'}
+    // ],
+    send: 'SEND A MESSAGE',
+    send2: 'SEND',
+    getintouch: 'Get in Touch',
+    respond: 'We will respond to your message as soon as possible',
+    example: 'Example Inc.',
+    paragraphright: [
+      {par: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
+      {par: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}
+    ],
+    map: 'VIEW MAP',
   }
 });
