4cc20afae4b771c7ca68f62bd341464fef7cc30a
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2021-03-06 18:15:27 +0100

    Sistemazione ultimi dettagli2

--- js/myscript.js
+++ js/myscript.js
@@ -1,50 +1,46 @@
 var app = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    icons: [
-      {clock: 'fas fa-clock'},
-      {facebook: 'fab fa-facebook-f'},
-      {twitter: 'fab fa-twitter'},
-      {instagram: 'fab fa-instagram'},
-      {phone: 'fas fa-phone-alt'},
-      {envelope: 'fas fa-envelope'},
-      {position: 'fas fa-map-marked'}
-    ],
-    hours: 'Open Hours: Mon - Sat - 9:00 - 18:00',
-    info: [
-      {message: '+1(305)1234-5678'},
-      {message: 'hello@example.com'},
-      {message: 'Main Avenue, 987'}
-    ],
-    navimage: 'img/bg-4.jpg',
-    nexgen: 'NEXGEN',
+    icons: {
+      clock: "fas fa-clock",
+      facebook: "fab fa-facebook-f",
+      twitter: "fab fa-twitter",
+      instagram: "fab fa-instagram",
+      phone: "fas fa-phone-alt",
+      envelope: "fas fa-envelope",
+      position: "fas fa-map-marked",
+    },
+    hours: "Open Hours: Mon - Sat - 9:00 - 18:00",
+    info: {
+      number: "+1(305)1234-5678",
+      mail: "hello@example.com",
+      address: "Main Avenue, 987",
+    },
+    navimage: "img/bg-4.jpg",
+    nexgen: "NEXGEN",
     navtext: [
-      {text: 'HOME'},
-      {text: 'ABOUT'},
-      {text: 'PROJECTS'},
-      {text: 'PROCESS'},
-      {text: 'TESTIMONIALS'}
+      { text: "HOME" },
+      { text: "ABOUT" },
+      { text: "PROJECTS" },
+      { text: "PROCESS" },
+      { text: "TESTIMONIALS" },
     ],
-    touch: 'GET IN TOUCH',
-    humanresources: 'HUMAN RESOURCES',
-    moreproductivity: 'Get More Productivity',
-    readmore: 'READ MORE',
-    planning: 'Planning, recruitment and selection process and performance evaluation of employees.',
-    user: 'fas fa-user',
-    // paragraphleft: [
-    //   {par: 'SEND A MESSAGE'},
-    //   {par: 'Get in Touch'},
-    //   {par: 'We will respond to your message as soon as possible'}
-    // ],
-    send: 'SEND A MESSAGE',
-    send2: 'SEND',
-    getintouch: 'Get in Touch',
-    respond: 'We will respond to your message as soon as possible',
-    example: 'Example Inc.',
+    touch: "GET IN TOUCH",
+    humanresources: "HUMAN RESOURCES",
+    moreproductivity: "Get More Productivity",
+    readmore: "READ MORE",
+    planning:
+      "Planning, recruitment and selection process and performance evaluation of employees.",
+    user: "fas fa-user",
+    send: "SEND A MESSAGE",
+    send2: "SEND",
+    getintouch: "Get in Touch",
+    respond: "We will respond to your message as soon as possible",
+    example: "Example Inc.",
     paragraphright: [
-      {par: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
-      {par: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}
+      { par: "Lorem ipsum dolor sit amet, consectetur adipisicing elit." },
+      { par: "Lorem ipsum dolor sit amet, consectetur adipisicing elit." },
     ],
-    map: 'VIEW MAP',
-  }
+    map: "VIEW MAP",
+  },
 });
